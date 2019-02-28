bfd9f57f3c1b20b1c67f201106316d78789e6220
kdttung18it1 <48077466+kdttung18it1@users.noreply.github.com>
2019-02-28 19:25:55 +0700

    Add files via upload

---
+++ baitap1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class tinhtong
+    {  //cau a
+        public int a(int n)
+        {
+            int s;
+
+            if (n % 2 == 0)
+                s = -n / 2;
+            else s = (n / 2) + 1;
+            return s;
+        }
+        //cau b
+        public float b(int n)
+        {
+            float s = 0;
+            for (float i = 1; i <= n; i++)
+            { s = s + (1 / i); }
+            return s;
+        }
+        //cau c
+        public int c(int n)
+        {
+            int s = 0;
+            for (int i = 1; i <= n; i++)
+                s = s + i;
+            return s;
+        }
+        //cau d
+        public int d(int n)
+        {
+            int s = 0;
+            for (int i = 1; i < n; i++)
+                if (n % i == 0)
+                    s = s + i;
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap vao n:\n");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tong cau a la:\t{0}", a(n));
+            Console.Write("Nhap vao n cau b:\n");
+            Console.WriteLine("Tong cau b la:\t{0}", b(n));
+            Console.Write("Nhap vao n cau c:\n");
+            Console.WriteLine("Tong cau c la:\t{0}", c(n));
+            Console.Write("Nhap vao n cau d:\n");
+            Console.WriteLine("Tong cau d la:\t{0}", d(n));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baitap2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Doantau
+    {
+        int khach;
+        string matau;
+        int tien;
+        public void nhap()
+        {
+            Console.WriteLine("Nhap ma tau:\t");
+            this.matau = Console.ReadLine();
+            Console.WriteLine("Nhap so luong khach:\t");
+            this.khach = Convert.ToInt32(Console.ReadLine());
+        }
+        public void intt()
+        {
+            Console.WriteLine("Ma tau {0} so tien la {1}", this.matau, this.tien);
+        }
+        public void tinhtien()
+        {
+            if (this.khach < 100)
+                this.tien = this.khach * 100000;
+            else this.tien = this.khach * 80000;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Doantau s1 = new Doantau();
+            s1.nhap();
+            s1.tinhtien();
+            s1.intt();
+            Console.ReadKey();
+        }
+    }
+}
